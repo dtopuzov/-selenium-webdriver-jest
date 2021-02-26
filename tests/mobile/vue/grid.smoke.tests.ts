bded434dbe8b958b332ded23b744795a90201f11
Dimitar Topuzov <dtopuzov@gmail.com>
2021-02-26 16:47:40 +0200

    fix: test

--- tests/mobile/vue/grid.smoke.tests.ts
+++ tests/mobile/vue/grid.smoke.tests.ts
@@ -18,7 +18,7 @@ it("should be able to sort", async () => {
     await browser.navigateTo(`${Config.vueUrl}/grid/examples/sorting/custom/?theme=default`);
     grid = new Grid(browser.driver);
 
-    const header = await grid.HeaderByText("ID");
+    const header = await grid.HeaderByText("ProductID");
     expect(await isAscending(await grid.CellsByColumn(2, 10))).toBe(true);
 
     await header.click();
