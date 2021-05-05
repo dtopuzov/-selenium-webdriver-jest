65902c682e8abfe18aef4da9dc1a8cfa7f81c849
Dimitar Topuzov <dtopuzov@gmail.com>
2021-05-05 19:27:11 +0300

    chore:  better logs when fail to find element

--- src/components/ui-component.ts
+++ src/components/ui-component.ts
@@ -7,20 +7,23 @@ export class UIComponent {
     }
 
     public async getRootElement(waitForVisible = true): Promise<WebElement> {
+        const findFailedError = `Failed to find element located by ${this.locator}.`;
+        const notVisibleError = `Element located by ${this.locator} is not visible.`;
+
         if (this.parentElement != undefined) {
             const element = this.parentElement.findElement(this.locator);
-            await this.driver.wait(until.elementIsVisible(element), Settings.timeout);
+            await this.driver.wait(until.elementIsVisible(element), Settings.timeout, notVisibleError);
             return element;
         }
         else {
-            const element = this.driver.wait(until.elementLocated(this.locator), Settings.timeout);
+            const element = this.driver.wait(until.elementLocated(this.locator), Settings.timeout, findFailedError);
             if (waitForVisible) {
-                await this.driver.wait(until.elementIsVisible(element), Settings.timeout);
+                await this.driver.wait(until.elementIsVisible(element), Settings.timeout, notVisibleError);
 
                 // Hack to handle Safari
                 const browserName = (await (await this.driver).getCapabilities()).getBrowserName().toLowerCase();
                 if (browserName == "safari") {
-                    await this.driver.wait(async () => await element.getRect() != undefined, Settings.timeout);
+                    await this.driver.wait(async () => await element.getRect() != undefined, Settings.timeout, notVisibleError);
                 }
             }
             return element;
